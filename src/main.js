5ce5ed686f27d9f135cfeb609cd1da2ab980cf0c
Gabriel Fernandes <gabriel.santos@before.com.br>
2019-09-18 10:30:38 -0400

    O fronte ae

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuelidate from './plugins/vuelidate'
+import vuetify from './plugins/vuetify'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
+  vuelidate,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/modules/auth/views/Login.vue
@@ -0,0 +1,182 @@
+<template>
+  <v-container fill-height>
+    <v-layout justify-center align-center>
+
+      <v-flex xs12 sm6 md4 lg3 xl3>
+        <v-card class="elevation-12">
+
+          <v-toolbar color="primary">
+            <v-toobar-title>{{ texts.toolbar }}</v-toobar-title>
+            <v-spacer></v-spacer>
+            <v-progress-circular
+              v-show="isLoading"
+              indeterminate
+              color="white"
+              width="2"
+            >
+            </v-progress-circular>
+          </v-toolbar>
+
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                v-if="!isLogin"
+                prepend-icon="person"
+                name="name"
+                label="Nome"
+                type="text"
+                :error-messages="nameErrors"
+                :success="!$v.user.name.$invalid"
+                v-model.trim="$v.user.name.$model"
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="email"
+                name="email"
+                label="Email"
+                type="email"
+                :error-messages="emailErrors"
+                :success="!$v.user.email.$invalid"
+                v-model.trim="$v.user.email.$model"
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                label="Senha"
+                type="password"
+                :error-messages="passwordErrors"
+                :success="!$v.user.password.$invalid"
+                v-model.trim="$v.user.password.$model"
+              ></v-text-field>
+            </v-form>
+            <v-btn
+              block
+              depressed
+              color="secondary"
+              @click="isLogin = !isLogin"
+            >{{ texts.bottom }}</v-btn>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              :disabled="$v.$invalid"
+              color="primary"
+              large
+              @click="submit()"
+            >{{ texts.toolbar }}</v-btn>
+          </v-card-actions>
+
+        <v-snackbar 
+        v-model="showSnackbar"
+        top
+        >
+          {{ error }}
+          <v-btn
+          color="pink"
+          flat
+          icon
+          @click="showSnackbar = false"
+          ></v-btn>
+          <v-icon>close</v-icon>
+        </v-snackbar>
+
+        </v-card>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import { required, email, minLength } from 'vuelidate/lib/validators'
+import AuthService from './../services/auth-service'
+import { formatError } from '@/utils'
+
+export default {
+  name: 'Login',
+  data: () => ({
+    isLogin: true,
+    isLoading: false,
+    showSnackbar: false,
+    user: {
+      name:'',
+      email: '',
+      password: ''
+    }
+  }),
+  validations() {
+    const validations = {
+      user: {
+        email: {
+          required,
+          email
+        },
+        password: {
+          required,
+          minLength: minLength(6)
+        }
+      }
+    }
+    if(this.isLogin){return validations}
+
+    return {
+      user: {
+        ...validations.user,
+        name: {
+          required,
+          minLength: minLength(3)
+        }
+      }
+    }
+  },
+  computed: {
+    texts () {
+      return this.isLogin
+        ? { toolbar: 'Entrar', button: 'Criar Conta' }
+        : { toolbar: 'Criar Conta', button: 'Ja tenho uma conta' }
+    },
+    emailErrors () {
+      const errors = []
+      const email = this.$v.user.email
+      if (!email.$dirty){ return errors }
+      !email.required && errors.push('Email � obrigat�rio!')
+      !email.email && errors.push('Insira um email v�lido!')
+      return errors
+    },
+    nameErrors () {
+      const errors = []
+      const password = this.$v.user.name
+      if (!name.$dirty){ return errors }
+      !name.required && errors.push('O Nome � obrigat�rio!')
+      !name.minLength && errors.push('Insira pelo menos ${name.$params.minLength.min} caracteres!')
+      return errors
+    },
+    passwordErrors () {
+      const errors = []
+      const password = this.$v.user.password
+      if (!password.$dirty){ return errors }
+      !password.required && errors.push('Senha � obrigat�ria!')
+      !password.minLength && errors.push('Insira pelo menos ${password.$params.minLength.min} caracteres!')
+      return errors
+    }
+  },
+  methods: {
+    async submit() {
+      this.isLoading = true
+      try{
+        this.isLogin
+        ? await AuthService.login(this.user)
+        : await AuthService.signup(this.user)
+        this.$router.push(this.$route.query.redirect || '/dashboard')
+      } catch(error){
+        console.log(error)
+        this.error = formatError(error.message)
+        this.showSnackbar = true
+      }finally{
+        this.isLoading = false
+      }
+      
+    }
+  }
+}
+</script>
